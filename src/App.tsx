2051a2e19a1add1305194dd7b250fe62dc916fe6
uendelAraujoInfnet <uendel.araujo@al.infnet.edu.br>
2024-11-01 17:18:23 -0400

    Deletado arquivos desnecessarios advindos d uso do comando create-react-app , realizado instalacao do framework vite, conversao de jsx para tsx e configuracao de arquivos necessarios para um correto funcionamento

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      Olá
+    </div>
+  );
+}
+
+export default App;
